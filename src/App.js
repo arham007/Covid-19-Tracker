407efc05e1f4abe9da691e3f9c557fba844043a4
ArhamKhan <arhamkhancs99@gmail.com>
2021-01-04 15:08:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import Card from './Components/Card';
+import React, { useEffect, useState } from 'react';
+import Cards from './Components/Cards';
 import Countrypicker from './Components/Countrypicker';
 import Chart from './Components/Chart';
+import { fetchGlobal } from "./Api";
+
 
 function App() {
+  let [global,setGlobal]=useState({})
+  useEffect(()=>{
+    async function fetchApi(){
+   
+      setGlobal(await fetchGlobal())
+      
+    } 
+
+    fetchApi()
+  },[])
+  // console.log(global)
+  
   return (
    <>
    <div className="container">
-     <Card />
-     <Countrypicker />
-     <Chart />
+     <Cards data={global} />
+     {/* <Countrypicker />
+     <Chart /> */}
    </div>
    </>
   );

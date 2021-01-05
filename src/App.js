c5ae9318c144c225639d7d63f97baf3d71dc3f5c
ArhamKhan <arhamkhancs99@gmail.com>
2021-01-05 13:45:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Cards from './Components/Cards';
 import Countrypicker from './Components/Countrypicker';
 import Chart from './Components/Chart';
 import { fetchGlobal } from "./Api";
-
+import { Header } from "./Components/Header";
 
 function App() {
-  let [global,setGlobal]=useState({})
+  let [global,setGlobal]=useState({});
+  let [country,setCountry]=useState("");
   useEffect(()=>{
     async function fetchApi(){
    
@@ -15,9 +16,10 @@ function App() {
     } 
 
     fetchApi()
-  },[])
+  },[setGlobal])
  
   const handleChange=async (country)=>{
+    setCountry(country)
     const data=await fetchGlobal(country)
     setGlobal(data)
 
@@ -25,12 +27,13 @@ function App() {
   
   return (
    <>
+   <Header />
    <div className="container">
      <Cards data={global} />
      <br /><br />
      <Countrypicker handleChange={handleChange} />
      <br /><br />
-     <Chart />
+     <Chart global={global} country={country} />
    </div>
    </>
   );
